ec2ba8389596f85c8ab185657edfe4c8472cd048
Tyler Wanlass <hello@tdub.co>
2025-12-28 18:40:47 -0800

    build cleanup

--- game/vite.config.js
+++ game/vite.config.js
@@ -1,18 +1,5 @@
 import { defineConfig } from "vite";
 
-const kaplayCongrats = () => {
-    return {
-        name: "vite-plugin-kaplay-hello",
-        buildEnd() {
-            const line =
-                "---------------------------------------------------------";
-            const msg = `🦖 Awesome pal! Send your game to us:\n\n💎 Discord: https://discord.com/invite/aQ6RuQm3TF \n💖 Donate to KAPLAY: https://opencollective.com/kaplay\n\ (you can disable this msg on vite.config)`;
-
-            process.stdout.write(`\n${line}\n${msg}\n${line}\n`);
-        },
-    };
-};
-
 export default defineConfig({
     // index.html out file will start with a relative path for script
     base: "./",
@@ -20,8 +7,8 @@ export default defineConfig({
         port: 3001,
     },
     build: {
-        // disable this for low bundle sizes
-        sourcemap: true,
+        // disable source maps in production for smaller bundle
+        sourcemap: false,
         rollupOptions: {
             output: {
                 manualChunks: {
@@ -30,8 +17,4 @@ export default defineConfig({
             },
         },
     },
-    plugins: [
-        // Disable messages removing this line
-        kaplayCongrats(),
-    ],
 });
